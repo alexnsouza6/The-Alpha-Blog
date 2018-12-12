d5ddff31f3a1f7f125d018e4b6da7c1cb4b3f474
Vinicius <viniciuscosta0197@gmail.com>
2018-12-12 10:48:46 -0200

    refactor/Session verification

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2018_11_16_023823) do
 
   # These are extensions that must be enabled in order to support this database
@@ -22,7 +21,6 @@ ActiveRecord::Schema.define(version: 2018_11_16_023823) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
-
   end
 
 end
