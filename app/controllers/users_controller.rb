063c40ce3a8ba8860c3449e96cace41ef5cc04c6
alexnsouza6 <alexnsouza6@hotmail.com>
2019-01-08 22:43:25 -0200

    feat: redirecting if user is updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,6 +32,7 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
       if @user.update(user_params)
         flash[:success] = 'Your account was successfully updated'
+        redirect_to users_path
       else
         render 'edit'
       end
