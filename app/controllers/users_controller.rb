31354b7ce09b84dd7aa968d8aa3e5b970c807d92
Vinicius <viniciuscosta0197@gmail.com>
2019-01-21 19:15:58 -0200

    feature: removing current user from index view and adding redirect after edit user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,6 +28,7 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
       if @user.update(user_params)
         flash[:success] = 'Your account was successfully updated'
+        redirect_to root_path
       else
         render 'edit'
       end
