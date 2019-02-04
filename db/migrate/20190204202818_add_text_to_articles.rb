d41d63a517705c12f7601530630c9852adba9cc6
Vinicius <viniciuscosta0197@gmail.com>
2019-02-04 18:42:07 -0200

    feature: only author or admin can delete articles

---
+++ db/migrate/20190204202818_add_text_to_articles.rb
@@ -0,0 +1,5 @@
+class AddTextToArticles < ActiveRecord::Migration[5.2]
+  def change
+    add_column :articles, :text, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_04_172350) do
+ActiveRecord::Schema.define(version: 2019_02_04_202840) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2019_02_04_172350) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "text"
+    t.string "author"
   end
 
   create_table "users", force: :cascade do |t|
